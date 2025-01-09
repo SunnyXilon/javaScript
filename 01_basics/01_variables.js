09cf51b0495597dad0a33196619c24c8f70b4be6
SunnyXilon <124437084+SunnyXilon@users.noreply.github.com>
2025-01-09 16:58:36 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,5 +4,5 @@ var accountPassword = "12345"
 accountCity = "jaipur"
 let accountState;
 console.log(accountId);
-
+c
 console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
