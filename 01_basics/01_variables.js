bba1c625ea77b61bd4d68530d811dce13db7d357
SunnyXilon <124437084+SunnyXilon@users.noreply.github.com>
2025-01-09 16:25:58 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12334
+let accountEmail = "sunny@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
